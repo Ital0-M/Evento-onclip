d853b7c196914e2b02de14afb6dd429018c7c17c
Ital0-M <italo.martinez.guerra@gmail.com>
2025-08-14 11:38:02 -0400

    primer commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let botonInicio = document.querySelector(".Inicio");
+
+    botonInicio.addEventListener("click", function () {
+        if (botonInicio.textContent === "Iniciar Sesion") {
+            botonInicio.textContent = "Cerrar Sesion";
+        } 
+        else {
+            botonInicio.textContent = "Iniciar Sesion";
+        }
+    });
+let botonDerecha =document.querySelector(".botonDerecha");
+    botonDerecha.addEventListener("click", function(){
+        botonDerecha.style.display = "none";
+    });
+    
+let botonLikeGato = document.querySelector(".botonIzquierda.superior");
+    botonLikeGato.addEventListener("click", function () {
+        let numero = parseInt(botonLikeGato.childNodes[0].textContent.trim());
+        numero++;
+        botonLikeGato.childNodes[0].textContent = numero + " ";
+    });
+
+let botonLikeGolden = document.querySelector(".botonIzquierda.inferior");
+    botonLikeGolden.addEventListener("click", function () {
+        let numero = parseInt(botonLikeGolden.childNodes[0].textContent.trim());
+        numero++;
+        botonLikeGolden.childNodes[0].textContent = numero + " ";
+    });
